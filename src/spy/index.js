f6daa8792a89ad899a8847b4100c24d8dbbea15d
André Padez <andre.padez@gmail.com>
2015-09-22 14:20:57 +0200

    git-spy.connect returns a promise now

--- src/spy/index.js
+++ src/spy/index.js
@@ -9,20 +9,21 @@ var spy = module.exports = {
   subscriptions: subscriptions,
   connect: function (config) {
     githubApi.authenticate(config)
-    githubApi.init()
+    return githubApi.init()
       .then(function (resp) {
         log.info('githubApi initialized')
       })
       .catch(function (err) {
         log.warn('githubApi failed to initialize', err)
       })
-
-    server.start(config)
       .then(function () {
-        log.info('git-spy', 'success!!!')
-      })
-      .catch(function (err) {
-        log.error('git-spy', 'failure to start', err)
+        return server.start(config)
+          .then(function () {
+            log.info('git-spy', 'success!!!')
+          })
+          .catch(function (err) {
+            log.error('git-spy', 'failure to start', err)
+          })
       })
   },
   match: function (hookshotData) {
