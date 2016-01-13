3ff1754611fa4c73f3a8d3b753c7e691b4ddd51d
Shawn Inder <shawn@vigour.io>
2016-01-12 21:15:36 -0500

    Add config port to allow tests to run

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var ip = require('ip')
 
 var config = module.exports = {
+  testPort: 60111,
   port: process.env.GITSPY_PORT,
   owner: process.env.GITSPY_OWNER || 'vigour-io',
   apiToken: process.env.GITSPY_API_TOKEN
